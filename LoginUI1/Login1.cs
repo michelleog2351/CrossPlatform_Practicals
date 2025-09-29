ca1b00734d3781586a3dc50dbcc1e63d2691ccf5
michelleog2351 <L00176523@atu.ie>
2025-09-29 11:32:55 +0100

    add LoginUI folder to the repo

---
+++ LoginUI1/Login1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginUI1;
+
+	public class Login1 : ContentPage
+	{
+		Button loginButton;
+		VerticalStackLayout layout;
+
+		public Login1()
+		{
+			this.BackgroundColor = Color.FromArgb("512bdf");
+
+			layout = new VerticalStackLayout
+			{
+				Margin = new Thickness(15, 15, 15, 15),
+				Padding = new Thickness(30, 60, 30, 30),
+				Children =
+				{
+					new Label { Text = "Please log in", FontSize = 30, TextColor = Color.FromRgb(255, 255, 100) },
+					new Label { Text = "Please log in", FontSize = 30, TextColor = Color.FromRgb(255, 255, 100) },
+					new Entry(),
+					new Label { Text = "Password", TextColor = Color.FromRgb(255, 255, 255) },
+					new Entry { IsPassword = true }
+				}
+
+			};
+		}
+		
+	}
+
